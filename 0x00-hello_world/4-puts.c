a028c99cc2c8681a63a20748ed0322e55199e8a0
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-06-30 13:57:17 -0700

    Hellp, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 int main(void)
 {
 
+
+puts("\"Programming is like building a multilingual puzzle");
 /*
  * Program to test the working of Puts command
  */
-
-puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 }
