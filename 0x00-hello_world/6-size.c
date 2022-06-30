eaf3690ab030de78709ae7d514c0bdaf083ce786
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-06-30 15:30:50 -0700

    SizeOf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+        int i;
+        double d;
+        char c;
+
+        printf("The size of Integer is: %lu.\n"), (unsigned long)sizeof(i);
+        printf("The size of Double is: %lu.\n"), (unsigned long)sizeof(d);
+        printf("The size of Char is: %lu.\n"), (unsigned long)sizeof(c);
+
+return (0);
+}	
