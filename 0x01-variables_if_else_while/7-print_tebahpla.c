6e6ccf2ccf65e0d1e707b414eef3dc9c71db53fc
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-01 16:41:13 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+OAOAOA  *  main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+	int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+{
+
+	putchar(c);
+}
+
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+}
