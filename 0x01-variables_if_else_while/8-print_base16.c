9a533d1ec8c5ddccb8862a7faaea6261e80c57f9
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-01 16:48:32 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  *  main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+	int main(void)
+{
+	char c;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+{
+
+	putchar(n);
+}
+	for (c = 'a'; c <= 'f'; c++)
+{
+
+	putchar(c);
+}
+
+	putchar(10);
+
+	return (0);
+}
