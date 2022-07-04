e27960afb3bd41e43ab76191df88edc32a0fb998
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-04 06:52:45 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main -> Alphabet printing
+ *Non empty comment line
+ *Return: Always 0 (Success)
+ */
+	int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 48; c <= 57; c++)
+	for (d = 48; d <= 57; d++)
+{
+
+	putchar(c);
+	putchar(d);
+	if (c != 57 && d !=57)
+{
+	putchar(44);
+	putchar(32);
+}
+
+}
+	putchar(10); /* this is an ascii code for new line*/
+	return (0);
+}
