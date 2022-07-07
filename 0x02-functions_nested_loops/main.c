e4e2d02c67316a040696c6d81f60f1be9f68356f
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-07 03:14:53 -0700

    New Corrections

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
+	void print_alphabet_x10(void);
 	int _islower(int c);
 	int _isalpha(int c);
 	int print_sign(int n);
@@ -17,7 +18,6 @@ int main(void)
 	int add(int a, int b);
 	void Print_to_98(int n);
 	void print_times_table(int n);
-	void print_alphabet_x10(void);
 	return (0);
 }
 
