a3b9dd7e0c6333d42ba5da57a6db9eeef30d3536
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-12 13:34:00 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * strlen - Function that prints the length of a character
+ * @s: is the varaible that stores the character
+ * return nothing
+ */
+int_strlen(char *s)
+{
+	strlen(*s)
+}
