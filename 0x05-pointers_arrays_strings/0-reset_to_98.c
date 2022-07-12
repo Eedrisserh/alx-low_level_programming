db5b6f831af3dda0e1cb60f6e6fd325e24a31690
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-12 12:14:01 -0700

    98 Baker Street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Function to change a variable value
+ * to 98 using pointer.
+ * @n: is the pointer varaible
+ * return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
