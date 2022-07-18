15a75079db7d55e9b11d8dfe6a3b0ae4068963e0
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-18 15:15:40 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * set_string - change a string
+ * @s: destination address
+ * @to: source address
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
