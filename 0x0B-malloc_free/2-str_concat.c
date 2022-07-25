d44f8cc908b906386ae0c0376dfbd488e96f41e8
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-25 06:28:39 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * str_concat - Function that concatenates two strings
+ * @s1: Is the source string
+ * @s2: Is the destination string
+ * Return: On success return set of the strings else return 0
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	s2 = malloc(sizeof(char) * strlen(s1));
+	strcat(s2, s1);
+	return (s2);
+}
