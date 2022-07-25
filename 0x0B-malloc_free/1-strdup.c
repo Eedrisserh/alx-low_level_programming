828161c760f43c1d3411684e61ac35b2a41bdc51
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-07-25 04:26:47 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 
 /**
  * _strdup - Function that duplicates a string
- * @str - Source string
+ * @str- Source string
  * @dest - Destination string
  * Return: returns 0
  */
@@ -16,9 +16,3 @@ char *_strdup(char *str)
 	dest = strdup(str);
 	return (dest);
 }
-
-
-
-
-
-
