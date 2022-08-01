d1feb40c074e23b004731b5a4ae16632dbe51a69
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-08-01 07:21:01 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - new struct for poppy
+ * @name: stores name
+ * @age: stores age in float
+ * @owner: stores the name of the owner
+ */
+
 struct dog
 {
 	char *name;
