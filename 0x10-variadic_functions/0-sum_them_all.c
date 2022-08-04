c6dc8f66516620798fa84a804abc01f7db3d1714
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-08-04 14:53:22 -0700

    To be is to be the value of a variable

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Function to sum all integers using variadic
+ * @n : Number of arguments to be computed
+ * Return: sum of the arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i;
+	int sum = 0;
+	va_list list;
+
+	if (n == 0)
+		return;
+	va_start(list, n);
+	for (i = 0; i <= n; i++)
+		sum = sum + va_arg(list, int);
+	va_end(list);
+
+	return (sum);
+}
