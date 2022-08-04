51ff8d7493a71e5894b59851f0b215bc51fe1fec
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-08-04 15:10:33 -0700

    To be is to be the value of a variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,8 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
+
 	for (i = 0; i <= n; i++)
 		sum = sum + va_arg(list, int);
+
 	va_end(list);
 
 	return (sum);
